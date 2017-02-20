9ab8a2870f10dc6debe48f7f06c0e06e116bd032
Joel W. King <joel.king@wwt.com>
2017-02-20 09:28:44 -0500

    v1.1 of atc.sh

--- atc.sh
+++ atc.sh
@@ -6,10 +6,21 @@
 #    All rights reserved.
 #    author: Joel W. King, World Wide Technology
 #    Revision history:
-#      - 17 February 2017  | 1.1  | Initial release
+#      - 17 February 2017  | 1.1  | Initial 
+#      - 20 February 2017  | 1.2  | When running under CliQr, install some dependancies 
 #
 echo "CloudCenter_EXTERNAL_SERVICE_LOG_MSG_START"
 echo $0 $1
+#
+#
+#
+if [ -f /etc/lsb-release ]; then
+    echo "Assume running in Ubuntu development environment"
+else 
+    echo "Assume running in CliQr production environment"
+    yum install git -y
+    easy_install requests
+fi
 
 git clone https://github.com/joelwking/ansible-cliqr
 
